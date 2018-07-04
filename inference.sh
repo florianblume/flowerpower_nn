aba58fca1def7ef1bbe2569b01f98a34dbddd16b
Florian Blume <flob.91@gmx.de>
2018-07-04 12:07:36 +0200

    Removed inference script.

--- inference.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-model=$1
-experiment=$2
-
-eval $"python python/inference_raw.py --config ./data/experiments/${model}/${experiment}/inference_val/config.json"
-eval $"python python/inference_pos.py --config ./data/experiments/${model}/${experiment}/inference_val/config.json"
-eval $"python python/inference_raw.py --config ./data/experiments/${model}/${experiment}/inference_test/config.json"
-eval $"python python/inference_pos.py --config ./data/experiments/${model}/${experiment}/inference_test/config.json"
